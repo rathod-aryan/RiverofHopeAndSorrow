5820d7e9bd4f6756e39ed924e616b2efc57dd974
Prakhar Rajpali <rajpaliprakhar23@gmail.com>
2021-08-13 22:39:58 +0530

    changes

--- index.js
+++ index.js
@@ -73,6 +73,6 @@ app.get("/about", (req, res) => {
 });
 app.use("/projects", projectRouter);
 
-app.listen(3000, process.env.IP, () => {
+app.listen(process.env.PORT || 3000, process.env.IP, () => {
   console.log(`Server started at http://localhost:3000/`);
 });
